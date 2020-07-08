ffd789f5729785a0bd2f677c63aba4b85c2ada2a
Swapnil Waghmare <swapnilwaghm@cybage.com>
2020-07-08 23:09:10 +0530

    code modifications and graphs

---
+++ Covid19_Internal/Covid19_Internal/Layers/View Controllers/CustomCells/RadioButtonView.swift
@@ -0,0 +1,21 @@
+//
+//  RadioButtonView.swift
+//  Covid19_Internal
+//
+//  Created by Swapnil Waghm on 7/8/20.
+//  Copyright © 2020 Cybage. All rights reserved.
+//
+
+import UIKit
+
+class RadioButtonView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
